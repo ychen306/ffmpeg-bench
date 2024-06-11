7c620c7cd798c843a9e18af9fd143c73d8aecdd6
dking16 <d.the.king1616@gmail.com>
2024-06-11 14:52:02 -0400

    more kernels

--- bench.c
+++ bench.c
@@ -1,12 +1,20 @@
 #include <stdint.h>
 
 void ff_h264_idct_add(uint8_t *_dst, int16_t *_block, int stride);
+
 int32_t scalarproduct_and_madd_int16(int16_t *v1, const int16_t *v2,
                                               const int16_t *v3,
                                               int order, int mul);
+
 int32_t scalarproduct_and_madd_int32(int16_t *v1, const int32_t *v2,
                                               const int16_t *v3,
                                               int order, int mul);
+void quantize_bands(int *out, const float *in, const float *scaled,
+    int size, int is_signed, int maxval, const float Q34,
+    const float rounding);
+
+void ff_h264_luma_dc_dequant_idct(uint8_t *_dst, int16_t *_block, int stride);
+
 void bench_ff_h264_idct_add() {
   uint8_t dst[4 * 4];
   int16_t block[4 * 4];
@@ -47,6 +55,30 @@ void bench_scalarproduct_and_madd_int32() {
   }
   scalarproduct_and_madd_int16(v1, v2, v3, order, mul);
 }
+
+void bench_quantize_bands() {
+  int out[4*4];
+  float in[4*4];
+  float scaled[4*4];
+  int size;
+  int is_signed;
+  int maxval;
+  float Q34;
+  float rounding;
+  /// execute function here, not sure of values
+}
+
+void bench_ff_h264_luma_dc_dequant_idct() {
+  int16_t output[240]; //not sure, stride is 16 and the final x_offset is 10*stride, so it should be 15*stride=240
+  int16_t input[4 * 4];
+  int qmul = 16; // just chose a number
+  for (int i = 0; i < 16; i++) {
+    input[i] = i;
+  }
+
+}
+
+
 
 
 int main() {
