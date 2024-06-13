42da067922386c3fe448f9884d26202f938ee05e
dking16 <d.the.king1616@gmail.com>
2024-06-13 17:24:56 -0400

    New random tests and fixing buffer size issue

--- bench.c
+++ bench.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void ff_h264_idct_add(uint8_t *_dst, int16_t *_block, int stride);
 
@@ -15,18 +16,46 @@ void quantize_bands(int *out, const float *in, const float *scaled,
     const float rounding);
 
 void ff_h264_luma_dc_dequant_idct(int16_t *_output, int16_t *_input, int qmul);
+// should make neater and in better practice
+uint8_t rand8() {
+	return rand() % 256;
+}
+
+int16_t rand16() {
+	return rand() % (1<<16);
+}
+
+int16_t *rand_array_16(int num_elems) {
+	int16_t *arr = malloc(sizeof(int16_t) * num_elems);
+	for (int i = 0; i < num_elems; i++)
+		arr[i] = rand16();
+	return arr;
+}
+uint8_t *rand_array_8(int num_elems) {
+	uint8_t *arr = malloc(sizeof(uint8_t) * num_elems);
+	for (int i = 0; i < num_elems; i++)
+		arr[i] = rand8();
+	return arr;
+}
 
 void bench_ff_h264_idct_add() {
-  uint8_t dst[4 * 4];
-  int16_t block[4 * 4];
+  //uint8_t dst[4 * 4];
+  //int16_t block[4 * 4];
   int stride = 4;
 
-  for (int i = 0; i < 16; i++) {
-    dst[i] = i;
-    block[i] = i;
-  }
+  srand(42);
+
+  uint8_t *dst = rand_array_8(4 * 4);
+  int16_t *block = rand_array_16(4 * 4);
+  
 
   ff_h264_idct_add(dst, block, stride);
+  for (int i = 0; i < 16; i++)
+  	printf("%d ", dst[i]);
+  printf("\n");
+
+  free(dst);
+  free(block);
 }
 
 void bench_scalarproduct_and_madd_int16() {
@@ -61,7 +90,7 @@ void bench_quantize_bands() {
   int out[4*4];
   float in[4*4];
   float scaled[4*4];
-  int size;
+  int size = 16;
   int is_signed;
   int maxval;
   float Q34;
@@ -69,9 +98,11 @@ void bench_quantize_bands() {
   /// execute function here, not sure of values
 }
 
+
+// problem with array size for output?
 void bench_ff_h264_luma_dc_dequant_idct() {
-  int16_t output[240]; //not sure, stride is 16 and the final x_offset is 10*stride, so it should be 15*stride=240
-  int16_t input[4 * 4];
+  int16_t output[15 * 16 + 1]; //not sure, stride is 16 and the final x_offset is 10*stride, so it should be 15*stride=240
+  int16_t input[16];
   int qmul = 16; // just chose a number
   for (int i = 0; i < 16; i++) {
     input[i] = i;
@@ -85,7 +116,6 @@ void bench_ff_h264_luma_dc_dequant_idct() {
 
 
 int main() {
-  bench_ff_h264_idct_add();
   bench_scalarproduct_and_madd_int16();
   bench_scalarproduct_and_madd_int32();
   // bench_quantize_bands();
