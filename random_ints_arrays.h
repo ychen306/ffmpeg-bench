b22630f32460fbb990964913aecfa8c99b6ace0b
dking16 <d.the.king1616@gmail.com>
2024-06-17 17:01:34 -0400

    formatting

--- random_ints_arrays.h
+++ random_ints_arrays.h
@@ -2,15 +2,15 @@
 #define RANDOM_INTS_ARRAYS_H
 
 
-#define RAND_INT(B) static int ## B ## _t  (rand ## B)(){ \
+#define RAND_INT(B) static int ## B ## _t  (rand ## B)(){                       \
                 return rand() % (1<<B);}
 
-#define RAND_ARRAY(B) static int ## B ## _t *(rand_array_ ## B)(int size){\
-        int ## B ## _t *arr = malloc(sizeof(int ## B ## _t) * size);\
-        for (int i = 0; i < size; i++)\
-            arr[i] = (rand ## B)();\
-        return arr;\
-}   \
+#define RAND_ARRAY(B) static int ## B ## _t *(rand_array_ ## B)(int size){      \
+        int ## B ## _t *arr = malloc(sizeof(int ## B ## _t) * size);            \
+        for (int i = 0; i < size; i++)                                          \
+            arr[i] = (rand ## B)();                                             \
+        return arr;                                                             \
+}                                                                               \
 
 
 inline int *rand_array(int size) {
