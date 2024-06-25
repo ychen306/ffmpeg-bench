942fb14df70a0f84c56088633e1b215979a12a44
dking16 <d.the.king1616@gmail.com>
2024-06-24 20:52:37 -0400

    Changing to median

--- run-tests.py
+++ run-tests.py
@@ -1,31 +1,18 @@
 import subprocess
+import statistics
 # same order as reurn values of the functions
 kernels = ["scalarproduct_and_madd_int16", "scalarproduct_and_madd_int32", "ff_h264_idct_add", "ff_h264_luma_dc_dequant_idct", 
-           "ps_stereo_interpolate", "ps_stereo_interpolate_ipdopd", "h263_h_loop_filter", "h263_v_loop_filter"]
-# getting initial results
-output = subprocess.run("./bench", capture_output=True).stdout.split()
-output_novec = subprocess.run("./bench.novec", capture_output=True).stdout.split()
-output = [int(val) for val in output]
-output_novec = [int(val) for val in output_novec]
-num_tests = 10000
-anomaly_threshold = 10000
-anomalies = 0
+           "ps_stereo_interpolate", "ps_stereo_interpolate_ipdopd", "h263_h_loop_filter", "h263_v_loop_filter", "weight_h264_pixels16_8_c"]
+vectorized_throughput = {name: [] for name in kernels}
+non_vectorized_throughput = {name: [] for name in kernels}
+num_tests = 1000 #number of tests to be run
 for _ in range(num_tests):
     vec_val = [int(out) for out in subprocess.run("./bench", capture_output=True).stdout.split()]
     novec_val = [int(out) for out in subprocess.run("./bench.novec", capture_output=True).stdout.split()]
-    for i in range(len(output)):
-        if vec_val[i] < anomaly_threshold:
-            output[i] += vec_val[i]
-        else:
-            anomalies += 1
-            
-        if novec_val[i] < anomaly_threshold:
-            output_novec[i] += novec_val[i]
-        else:
-            anomalies += 1
-vectorized_throughput = [val/(num_tests+anomalies) for val in output]
-non_vectorized_throughput = [val/(num_tests+anomalies) for val in output_novec]
+    for ind, name in enumerate(kernels):
+        vectorized_throughput[name].append(vec_val[ind])
+        non_vectorized_throughput[name].append(novec_val[ind])
+
 print("Vectorized speedup")
-for i in range(len(kernels)):
-    print(f"{kernels[i]}: {non_vectorized_throughput[i]/vectorized_throughput[i]}")
-print(f"anomalies = {anomalies}")
+for name in kernels:
+    print(f"{name}: {statistics.median(non_vectorized_throughput[name])/statistics.median(vectorized_throughput[name])}")
